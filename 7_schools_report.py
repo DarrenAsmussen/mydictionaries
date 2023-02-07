b925409c32c0bdb256a1602ea0806c1cf8c6499c
Darren Asmussen <darren_asmussen1@baylor.edu>
2023-02-07 11:25:31 -0600

    School Report Update

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -40,3 +40,19 @@ for school in schools:
             )
             print()
             print()
+
+# Display report for all universities that have a total price for in-state students living off campus over $50,000
+
+for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if (
+            school[
+                "Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"
+            ]
+            > 50000
+        ):
+            print(f"University Name: {school['instnm']}")
+            print(
+                f"Total price for in-state students living off campus: ${school['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)']}"
+            )
+            print()
